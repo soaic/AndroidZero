e2a2e81180cf0e0d3b8ac9a6daf21c3404670acc
sxiao <sxiao@skystartrade.com>
2021-09-13 11:19:36 +0800

    update

---
+++ ButterKnife/src/main/java/com/soaic/butterknife/ButterKnife.java
@@ -0,0 +1,11 @@
+package com.soaic.butterknife;
+
+import android.app.Activity;
+
+public class ButterKnife {
+
+    public static void bind(Activity activity) {
+
+    }
+
+}
---
+++ app/src/main/java/com/soaic/zero/butterknife/MainActivity.java
@@ -0,0 +1,38 @@
+package com.soaic.zero.butterknife;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.soaic.annotations.BindView;
+import com.soaic.zero.R;
+
+
+public class MainActivity extends Activity {
+
+
+    @BindView(R.id.textView1)
+    TextView textView1;
+
+
+    @BindView(R.id.textView2)
+    TextView textView2;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_butterknife);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
