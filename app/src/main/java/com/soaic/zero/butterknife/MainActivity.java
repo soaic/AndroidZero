26a0909464f79c83146c25ad7cdb3158c9df22d3
xiaosai <xiaosai@uwintrader.com>
2024-02-22 19:06:13 +0800

    更新配置

--- app/src/main/java/com/soaic/zero/butterknife/MainActivity.java
+++ app/src/main/java/com/soaic/zero/butterknife/MainActivity.java
@@ -6,18 +6,18 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import com.soaic.annotations.BindView;
+//import com.soaic.annotations.BindView;
 import com.soaic.zero.R;
 
 
 public class MainActivity extends Activity {
 
 
-    @BindView(R.id.textView1)
+    //@BindView(R.id.textView1)
     TextView textView1;
 
 
-    @BindView(R.id.textView2)
+    //@BindView(R.id.textView2)
     TextView textView2;
 
 
